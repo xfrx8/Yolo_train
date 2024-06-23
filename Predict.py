db104c788077b48f4b3cbfcf4a777f57821901b9
xfrx8 <6221927013@stu.jiangnan.edu.cn>
2024-06-23 15:19:41 +0800

    feat: update method name to predict in Predict.py

--- Predict.py
+++ Predict.py
@@ -4,5 +4,5 @@ if __name__ == '__main__':
 
     # Initialize model
     model = YOLO('runs/detect/train27/weights/best.pt') # 初始化YOLO模型，加载预训练的模型文件'yolov8n.pt'
-    results = model.track(source='videos/视频1.mp4', imgsz=1280, conf=0.7, save=True)
+    results = model.predict(source='videos/视频1.mp4', imgsz=1280, conf=0.7, save=True)
 
